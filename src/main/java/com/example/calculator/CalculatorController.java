15d1dec398c8fac71cee936de9db650f4ed73649
safwan zullash <wi21b030@technikum-wien.at>
2022-06-02 16:54:14 +0200

    added commentary

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,37 +1,54 @@
 package com.example.calculator;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class CalculatorController {
 
     /**
-     * Does sum calculation
+     * does addition
      * @param a int
      * @param b int
      * @return a+b
      */
 
     //localhost:8080/sum?a=5&b=6 so kann man bspw die Funktion im Browser verwenden
+
     @RequestMapping("/sum")
     public int sum(@RequestParam int a,@RequestParam int b){
         return a + b;
     }
 
+    /**
+     * does subtraction
+     * @param a int
+     * @param b int
+     * @return a-b
+     */
     @RequestMapping("/sub")
     public int sub(@RequestParam int a,@RequestParam int b){
         return a - b;
     }
 
+    /**
+     * does multiplication
+     * @param a int
+     * @param b int
+     * @return a*b
+     */
     @RequestMapping("/mul")
     public int mul(@RequestParam int a,@RequestParam int b){
         return a * b;
     }
 
+    /**
+     * does division
+     * @param a
+     * @param b
+     * @return
+     */
     @RequestMapping("/div")
     public String div(@RequestParam int a,@RequestParam int b){
         if(b != 0){
@@ -40,16 +57,32 @@ public class CalculatorController {
         return "Division durch 0 nicht möglich";
     }
 
+    /**
+     * does multiplication with itself
+     * @param a
+     * @return
+     */
     @RequestMapping("/quad")
     public int quad(@RequestParam int a){
         return a * a;
     }
 
+    /**
+     * does multiplication to the power of b
+     * @param a
+     * @param b
+     * @return
+     */
     @RequestMapping("/pow")
     public int pow(@RequestParam int a,@RequestParam int b){
         return (int) Math.pow(a,b);
     }
 
+    /**
+     * does absolute of number
+     * @param a
+     * @return
+     */
     @RequestMapping("/abs")
     public int abs(@RequestParam int a){
         return Math.abs(a);
